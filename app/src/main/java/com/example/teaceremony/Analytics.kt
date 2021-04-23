209cc50cd8a7d07fdebf7014003a7a01cfa3f0bb
Olha <drozdik190696@gmail.com>
2021-04-23 11:47:16 +0300

    Rework

---
+++ app/src/main/java/com/example/teaceremony/Analytics.kt
@@ -0,0 +1,19 @@
+package com.example.teaceremony
+
+object Analytics {
+    object Events {
+        const val category_click = "category_click"
+        const val drink_click = "drink_click"
+        const val search_click = "search_click"
+
+        const val details_screen = "details_screen"
+    }
+
+    object Params {
+        const val type = "type"
+        const val drink_name = "drink_name"
+    }
+}
+
+
+
